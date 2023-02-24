e785afbdc84ba6f9605080000f4091262e97f72c
choisujin <98chltnwls@gmail.com>
2023-02-24 23:39:15 +0900

    2/24

--- 03_JAVASCRIPT/js/05_간이계산기_실습.js
+++ 03_JAVASCRIPT/js/05_간이계산기_실습.js
@@ -1,6 +1,44 @@
-function calc(){
-    switch(document.getElementsByTagName("button").value){
 
-    }
+// 전역 변수
+const num1 = document.getElementById("input1"); // 요소 추적 O, 값이나 속성 추적 X
+const num2 = document.getElementById("input2");
 
+
+const resultBox = document.getElementById("resultBox");
+let result;
+
+function plus(){
+
+            // 함수가 호출되었을 때 내부코드가 해석되면서 수행
+    result = Number(num1.value) + Number(num2.value);
+
+    resultBox.innerText = "계산 결과 : " + result;
+}
+
+function minus(){
+
+    result = Number(num1.value) - Number(num2.value);
+
+    resultBox.innerText = "계산 결과 : " + result;
+}
+
+function multi(){
+
+    result = Number(num1.value) * Number(num2.value);
+
+    resultBox.innerText = "계산 결과 : " + result;
+}
+
+function div(){
+
+    result = Number(num1.value) / Number(num2.value);
+
+    resultBox.innerText = "계산 결과 : " + result;
+}
+
+function rest(){
+
+    result = Number(num1.value) % Number(num2.value);
+
+    resultBox.innerText = "계산 결과 : " + result;
 }

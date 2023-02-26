6107b9f21f3c474cc96ae913b96b07d96462d256
CHOISUJIN <98chltnwls@gmail.com>
2023-02-27 01:52:44 +0900

    commit

--- 03_JAVASCRIPT/js/05_간이계산기3.js
+++ 03_JAVASCRIPT/js/05_간이계산기3.js
@@ -4,11 +4,15 @@ const num2 = document.getElementById("num2");
 
 const result = document.getElementById("result");
 
-function calc(op) {
+function calc(btn) {
   //op에는 +,-,*,/,% 중 하나가 전달됨
+
+  console.log(btn);
 
   const n1 = Number(num1.value);
   const n2 = Number(num2.value);
+
+  const op = btn.innerText; // 클릭된 버튼에 작성된 내용을 얻어옴 --> +,-,*,/,%
 
   switch (op) {
     case "+":
--- 03_JAVASCRIPT/js/05_간이계산기_실습.js
+++ 03_JAVASCRIPT/js/05_간이계산기_실습.js
@@ -1,44 +1,37 @@
-
 // 전역 변수
 const num1 = document.getElementById("input1"); // 요소 추적 O, 값이나 속성 추적 X
 const num2 = document.getElementById("input2");
 
-
 const resultBox = document.getElementById("resultBox");
 let result;
 
-function plus(){
+function plus() {
+  // 함수가 호출되었을 때 내부코드가 해석되면서 수행
+  result = Number(num1.value) + Number(num2.value);
 
-            // 함수가 호출되었을 때 내부코드가 해석되면서 수행
-    result = Number(num1.value) + Number(num2.value);
-
-    resultBox.innerText = "계산 결과 : " + result;
+  resultBox.innerHTML = "<br><br>계산 결과 : " + result;
 }
 
-function minus(){
+function minus() {
+  result = Number(num1.value) - Number(num2.value);
 
-    result = Number(num1.value) - Number(num2.value);
-
-    resultBox.innerText = "계산 결과 : " + result;
+  resultBox.innerHTML = "<br><br>계산 결과 : " + result;
 }
 
-function multi(){
+function multi() {
+  result = Number(num1.value) * Number(num2.value);
 
-    result = Number(num1.value) * Number(num2.value);
-
-    resultBox.innerText = "계산 결과 : " + result;
+  resultBox.innerHTML = "<br><br>계산 결과 : " + result;
 }
 
-function div(){
+function div() {
+  result = Number(num1.value) / Number(num2.value);
 
-    result = Number(num1.value) / Number(num2.value);
-
-    resultBox.innerText = "계산 결과 : " + result;
+  resultBox.innerHTML = "<br><br>계산 결과 : " + result;
 }
 
-function rest(){
+function rest() {
+  result = Number(num1.value) % Number(num2.value);
 
-    result = Number(num1.value) % Number(num2.value);
-
-    resultBox.innerText = "계산 결과 : " + result;
+  resultBox.innerHTML = "<br><br>계산 결과 : " + result;
 }

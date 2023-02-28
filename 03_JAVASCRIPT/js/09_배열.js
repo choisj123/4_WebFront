6681840e60df63de478f7abf4003e3368ce2b8ff
choisujin <98chltnwls@gmail.com>
2023-02-28 15:15:11 +0900

    git test

--- 03_JAVASCRIPT/js/09_배열.js
+++ 03_JAVASCRIPT/js/09_배열.js
@@ -4,7 +4,7 @@ document.getElementById("btn1").addEventListener("click", function () {
   const arr2 = new Array(3);
   const arr3 = [];
   const arr4 = ["사과", "딸기", "바나나"];
-
+// 깃 테스트
   console.log(arr1);
   console.log(arr2);
   console.log(arr3);

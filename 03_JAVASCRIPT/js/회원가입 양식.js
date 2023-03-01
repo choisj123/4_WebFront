89c90f001829673abad8c6ff3d4c24965148ef11
choisujin <98chltnwls@gmail.com>
2023-03-02 00:21:17 +0900

    3/1 과제

--- 03_JAVASCRIPT/js/회원가입 양식.js
+++ 03_JAVASCRIPT/js/회원가입 양식.js
@@ -1,50 +1,88 @@
-document.getElementById("id").addEventListener("keypress", function () {
-  // - 아이디 (영문 소문자 + 숫자 / 5글자 이상) - 아이디 사용 가능 / 불가능
-  const regEx = /[a-z][[0-9]{5,}/;
-  const idOutput = document.getElementById("idOutput").value;
+//아이디
+const userId = document.getElementById("userId");
+userId.addEventListener("keypress", function () {
+  // - 아이디 (영문 소문자로 시작, 영어 대/소문자, 숫자, -, _로만 이루어진 6~14글자 사이 문자열
+  const regEx = /^[a-z]+[a-zA-Z0-9-_]{6,14}$/g;
+  
 
   if (regEx.test(this.value)) {
-    idOutput.textContent = "아이디 사용 가능.";
-    idOutput.classList.add("confirm");
+    userId.style.backgroundColor = "springgreen";
   } else {
-    idOutput.innerText = "아이디 사용 불가능";
+    userId.style.backgroundColor = "red";
+    userId.style.color = "white";
+
   }
 });
 
-document.getElementById("password").addEventListener("keyup", function () {
-  // - 비밀번호 (영문대/소문자 + 특수문자 + 숫자 / 8글자 이상) - 비밀번호 사용 가능/불가능
-  const regEx = /[a-z][A-Z]{8,}/;
+// 비밀번호
+const userPw = document.getElementById("userPw");
+const userPwCheck = document.getElementById("userPwCheck");
 
-  if (regEx.test(this.value)) {
+const pwResult = document.getElementById("pwResult");
+pwResult.innerHTML = "";
+
+userPw.addEventListener("keyup", function () {
+  if(userPw = userPwCheck){
+    pwResult.innerHTML = "비밀번호 일치"
+    pwResult.style.color = "green";
+  }else{
+    pwResult.innerHTML = "비밀번호 불일치"
+    pwResult.style.color = "red";
   }
-});
-// - 비밀번호 확인 ( 비밀번호와 일치했을 때 일치한다 표시 )
+  
 
-document.getElementById("name").addEventListener("keyup", function () {
-  // - 이름 (한글 2글자 이상)
-
-  const regEx = /[가-힣]{2,}/;
 });
 
-document.getElementById("phone").addEventListener("keyup", function () {
-  // - 휴대폰번호 (숫자만 단, 010으로 시작)
-  const regEx = /^010[0-9]/;
+// - 이름 (한글 2~5글자)
+const userName = document.getElementById("userName")
+userName.addEventListener("keyup", function () {
+
+  const nameResult = document.getElementById("nameResult");
+
+  const regEx = /[가-힣]{2,5}/;
+  if(regEx.test(this.value)){
+    nameResult.innerHTML = "정상입력"
+    nameResult.style.color = "green";
+  }else{
+    nameResult.innerHTML = "한글만 입력하세요"
+    nameResult.style.color = "red";
+  }
+
 });
 
-document.getElementById("RRN-1").addEventListener("keyup", function () {
-  // [  6글자  ] - [       ]
-  const regEx = /{6}/;
-});
-document.getElementById("RRN-2").addEventListener("keyup", function () {
-  // - 주민등록번호 (단, 7번째 숫자는 1,2,3,4 만 가능)
-  const reqEx = /[1,2,3,4]/;
-});
-document.getElementById("email").addEventListener("keyup", function () {
-  // - 이메일 ( @무조건 포함. 맨뒤는 .com 으로 끝날 것)
-  const regEx = /@ .com$/;
-});
-// - submit 버튼
+// 회원가입 버튼 클릭시
+const register = document.getElementById("register");
+register.addEventListener("click", function(){
 
-// 둘다 가능일때
+  // 성별
+  const gender = document.getElementById("gender")
+  gender.addEventListener("keyup", function(){
+  
+    if(gender.value = null){
+      alert("성별을 선택해주세요")
+    }
+  })
 
-// alret 창 (" 사용 가능한 계정입니다 ! ") / (" 유효성 검사가 완료되지 않았습니다 ")
+  // 전화 번호
+  const phone = document.getElementById("phone")
+  phone.addEventListener("keyup", function () {
+    const regEx = /^0(?:\d{1}|\d{2})-(?:\d{3}|\d{4})-\d{4}$/;
+  
+    if (!regEx.test(this.value)) {
+      alert("전화번호의 형식이 올바르지 않습니다")
+    }
+  });
+
+
+  // 이메일
+  const email = document.getElementById("email")
+  email.addEventListener("keyup", function () {
+    const regEx = /@ .com$/;
+  });
+
+
+})
+
+
+
+
